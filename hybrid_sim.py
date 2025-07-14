186f1d2ebd15b42e6242165052d2cf0f59a25b52
Marcus <marcus.writesman@gmail.com>
2025-07-14 10:36:12 -0500

    MLKEM logic moved to other file

--- hybrid_sim.py
+++ hybrid_sim.py
@@ -1,11 +1,9 @@
-from quantcrypt.kem import MLKEM_1024
-from qunetsim.components import Host, Network
 from bb84 import BB84
-import base64
+from mlkem import MLKEM
+from qunetsim.components import Host, Network
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives import hashes
-
 
 NETWORK_TIMEOUT = 20
 
@@ -17,47 +15,27 @@ def setup_network():
     network = Network.get_instance()
     network.start()
 
-    hosts = {
-        'classical': Host('Classical Node'),
-        'middleware': Host('Middleware Node'),
-        'quantum': Host('Quantum Node')
-    }
+    host_classical = Host('Classical Node')
+    host_middleware = Host('Middleware Node')
+    host_quantum = Host('Quantum Node')
 
-    hosts['classical'].add_c_connection(hosts['middleware'].host_id)
-    hosts['middleware'].add_c_connection(hosts['classical'].host_id)
-    hosts['middleware'].add_connection(hosts['quantum'].host_id)
-    hosts['quantum'].add_connection(hosts['middleware'].host_id)
+    hosts = [host_classical, host_middleware, host_quantum]
 
-    for host in hosts.values():
+    host_classical.add_c_connection(host_middleware.host_id)
+    host_middleware.add_c_connection(host_classical.host_id)
+    host_middleware.add_connection(host_quantum.host_id)
+    host_quantum.add_connection(host_middleware.host_id)
+
+    network.add_hosts(hosts)
+
+    for host in hosts:
         host.start()
 
-    network.add_hosts(list(hosts.values()))
     return network, hosts
 
 
-def ml_kem_decapsulate(host: Host, sender_id: str, kem_instance):
-    public_key, secret_key = kem_instance.keygen()
-    host.send_classical(sender_id, public_key, await_ack=True)
-    ciphertext_obj = host.get_next_classical(sender_id, wait=-1)
-    if not ciphertext_obj:
-        return None
-    ciphertext = ciphertext_obj.content
-    shared_secret = kem_instance.decaps(secret_key, ciphertext)
-    return base64.urlsafe_b64encode(shared_secret)
-
-
-def ml_kem_encapsulate(host: Host, receiver_id: str, kem_instance):
-    public_key_obj = host.get_next_classical(receiver_id, wait=NETWORK_TIMEOUT)
-    if not public_key_obj:
-        return None
-    public_key = public_key_obj.content
-    ciphertext, shared_secret = kem_instance.encaps(public_key)
-    host.send_classical(receiver_id, ciphertext, await_ack=True)
-    return base64.urlsafe_b64encode(shared_secret)
-
-
-def classical_protocol(host: Host, middleware_id: str, kem_instance):
-    pqc_key = ml_kem_decapsulate(host, middleware_id, kem_instance)
+def classical_protocol(host: Host, middleware_id: str):
+    pqc_key = MLKEM.decapsulate(host, middleware_id)
 
     ciphertext_pqc = host.get_next_classical(middleware_id, wait=-1).content
 
@@ -77,8 +55,8 @@ def classical_protocol(host: Host, middleware_id: str, kem_instance):
     print(f"[{host.host_id}] Derived Key: {derived_key}")
 
 
-def middleware_protocol(host: Host, classical_id: str, quantum_id: str, kem_instance):
-    pqc_key = ml_kem_encapsulate(host, classical_id, kem_instance)
+def middleware_protocol(host: Host, classical_id: str, quantum_id: str):
+    pqc_key = MLKEM.encapsulate(host, classical_id)
 
     qkd_key = BB84.alice_protocol(host, quantum_id)
 
@@ -115,16 +93,13 @@ def quantum_protocol(host: Host, middleware_id: str):
 
 
 def main():
-    pqc_kem_instance = MLKEM_1024()
     network, hosts = setup_network()
 
-    classical_node = hosts['classical']
-    middleware_node = hosts['middleware']
-    quantum_node = hosts['quantum']
+    classical_node, middleware_node, quantum_node = hosts
 
-    thread_c = classical_node.run_protocol(classical_protocol, (middleware_node.host_id, pqc_kem_instance))
+    thread_c = classical_node.run_protocol(classical_protocol, (middleware_node.host_id,))
     thread_m = middleware_node.run_protocol(middleware_protocol,
-                                            (classical_node.host_id, quantum_node.host_id, pqc_kem_instance))
+                                            (classical_node.host_id, quantum_node.host_id))
     thread_q = quantum_node.run_protocol(quantum_protocol, (middleware_node.host_id,))
 
     for thread in [thread_c, thread_m, thread_q]:
